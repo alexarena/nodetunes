6186dbb52fa131bb027b10821a8eeace593e4233
Alex Arena <im@alexarena.com>
2020-05-04 12:16:14 -0700

    supports passing mdns as server option

--- examples/server.js
+++ examples/server.js
@@ -1,17 +1,18 @@
-'use strict';
+'use strict'
 
-var AirTunesServer = require('../index');
-var Speaker = require('speaker');
+var AirTunesServer = require('../index')
+var Speaker = require('speaker')
+var mdns = require('mdns')
 
 var speaker = new Speaker({
   channels: 2,
   bitDepth: 16,
   sampleRate: 44100,
-});
-var server = new AirTunesServer({ serverName: 'NodeTunes Speaker' });
+})
+var server = new AirTunesServer({ serverName: 'NodeTunes Speaker', mdns })
 
-server.on('clientConnected', function(stream) {
-  stream.pipe(speaker);
-});
+server.on('clientConnected', function (stream) {
+  stream.pipe(speaker)
+})
 
-server.start();
+server.start()
